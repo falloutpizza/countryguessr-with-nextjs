57a6af201df56f2e3afab8087658287f3d5b5579
falloutpizza <hashtag.punkisthebestintheworld@gmail.com>
2026-04-08 22:38:42 +0530

    added play button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <h1 className="sm:text-2xl md:text-4xl font-semibold mt-40">
         countryguessr dot com
       </h1>
+      <button className="m-5 text-sm md:text-lg px-3 py-1 bg-orange-400 border-1 rounded-3xl cursor-pointer">
+        play
+      </button>
     </>
   );
 }
