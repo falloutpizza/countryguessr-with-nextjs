fc95b7163049f2da8be3d0ebd0bed94c51e00617
falloutpizza <hashtag.punkisthebestintheworld@gmail.com>
2026-04-10 14:20:58 +0530

    changed import path

--- app/gamemodes/the-original-countryguessr/page.tsx
+++ app/gamemodes/the-original-countryguessr/page.tsx
@@ -1,7 +1,7 @@
 import { countReset } from "console";
 import fetchCountries from "../../services/fetchCountries";
 
-import QuestionMain from "@/app/ui/game-main/QuestionMain";
+import QuestionMain from "../../ui/game/QuestionMain";
 
 export default async function TheOriginalCountryguessr() {
   const countries = await fetchCountries();
