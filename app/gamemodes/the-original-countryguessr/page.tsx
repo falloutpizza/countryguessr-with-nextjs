6d3ede00f6fffc04380205357b6283d77656704d
falloutpizza <hashtag.punkisthebestintheworld@gmail.com>
2026-04-11 17:05:37 +0530

    added an end screen and end game button

--- app/gamemodes/the-original-countryguessr/page.tsx
+++ app/gamemodes/the-original-countryguessr/page.tsx
@@ -1,8 +1,7 @@
-import { countReset } from "console";
 import fetchCountries from "../../services/fetchCountries";
 import fetchRandomCountry from "@/app/utils/fetchRandomCountry";
 
-import QuestionMain from "../../ui/game/QuestionMain";
+import QuestionMain from "../../ui/game/the-og-countryguessr/QuestionMain";
 
 export default async function TheOriginalCountryguessr() {
   const countries = await fetchCountries();
---
+++ app/ui/game/the-og-countryguessr/QuestionMain.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { useState, useEffect } from "react";
+
+import CountryImage from "../CountryImage";
+import HintGroup from "../HintGroup";
+import InputGuess from "../InputGuess";
+import Results from "../Results";
+import NextQuestion from "../NextQuestion";
+import Score from "../Score";
+import EndMain from "./EndMain";
+import EndButton from "../EndButton";
+
+export default function QuestionMain({
+  countries,
+  initCountry,
+}: {
+  countries: Array<object>;
+  initCountry: any;
+}) {
+  const [country, setCountry] = useState(initCountry);
+  const [guessed, setGuessed] = useState("false");
+  const [totalScore, setTotalScore] = useState(0);
+  const [curScore, setCurScore] = useState(100);
+  const [ended, setEnded] = useState(false);
+
+  useEffect(() => {
+    if (guessed === "correct") {
+      setTotalScore(() => {
+        return totalScore + curScore;
+      });
+      setCurScore(100);
+    }
+  }, [guessed]);
+
+  useEffect(() => {
+    if (ended) {
+      setGuessed("false");
+      setCurScore(100);
+    }
+  }, [ended]);
+
+  return (
+    <div className="mt-15 mx-10 md:flex">
+      {!ended && (
+        <div className="md:w-1/2">
+          <Score
+            guessed={guessed}
+            totalScore={totalScore}
+            curScore={curScore}
+          />
+          <CountryImage src={country.image} />
+        </div>
+      )}
+      {!ended && (
+        <div className="md:w-1/2">
+          <h1 className="text-3xl font-semibold">guess the country!</h1>
+          <InputGuess
+            countries={countries}
+            countryName={country.name}
+            guessed={guessed}
+            setGuessed={setGuessed}
+          />
+          {guessed !== "false" && <Results guess={guessed} />}
+          <HintGroup country={country} setCurScore={setCurScore} />
+          <div className="flex justify-center">
+            <NextQuestion
+              countries={countries}
+              setCountry={setCountry}
+              guessed={guessed}
+              setGuessed={setGuessed}
+            />
+            <EndButton
+              setEnded={setEnded}
+              setScore={setTotalScore}
+              countries={countries}
+              setCountry={setCountry}
+            />
+          </div>
+        </div>
+      )}
+      {ended && <EndMain score={totalScore} setEnded={setEnded} />}
+    </div>
+  );
+}
